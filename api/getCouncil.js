b8187af6d7d6763be2b499649b1d957782fb64f5
j-dog22-cmd <joby@disco2000.com>
2026-01-31 18:00:21 +0000

    Update getCouncil.js

--- api/getCouncil.js
+++ api/getCouncil.js
@@ -30,7 +30,6 @@ export default async function handler(req, res) {
       council
     });
   } catch (err) {
-    console.error(err);
     return res.status(500).json({
       error: "Server error",
       details: err.message
